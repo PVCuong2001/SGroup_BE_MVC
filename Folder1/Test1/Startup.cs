40418a5981c918ac081fb8c308adf12efd0ef3f5
PVCuong2001 <cuongphanit2001@gmail.com>
2021-06-25 23:08:06 +0700

    len la len la len Docker part1 :3

--- Folder1/Test1/Startup.cs
+++ Folder1/Test1/Startup.cs
@@ -90,7 +90,7 @@ namespace Test1
             }
             else
             {
-                app.UseExceptionHandler();
+                app.UseExceptionHandler("/error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
